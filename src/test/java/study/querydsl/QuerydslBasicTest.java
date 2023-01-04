d0d5c4ddfa5950d768d1b669eab8110d69b04e67
yh725 <yh725@kakao.com>
2023-01-05 06:21:45 +0900

    기본 문법 - 기본 Q-Type 활용 - 검색 조건 쿼리

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -14,6 +14,7 @@ import study.querydsl.entity.Team;
 import javax.persistence.EntityManager;
 
 import static org.assertj.core.api.Assertions.*;
+import static study.querydsl.entity.QMember.*;
 
 @SpringBootTest
 @Transactional
@@ -59,12 +60,36 @@ public class QuerydslBasicTest {
 	@Test
 	public void startQuerydsl() {
 //		JPAQueryFactory queryFactory = new JPAQueryFactory(em);
-		QMember m = new QMember("m");
+//		QMember m = QMember.member;
 
 		Member findMember = queryFactory
-				.select(m)
-				.from(m)
-				.where(m.username.eq("member1")) //파라미터 바인딩 처리
+				.select(member)
+				.from(member)
+				.where(member.username.eq("member1")) //파라미터 바인딩 처리
+				.fetchOne();
+
+		assertThat(findMember.getUsername()).isEqualTo("member1");
+	}
+
+	@Test
+	public void search() {
+		Member findMember = queryFactory
+				.selectFrom(member)
+				.where(member.username.eq("member1").
+						and(member.age.eq(10)))
+				.fetchOne();
+
+		assertThat(findMember.getUsername()).isEqualTo("member1");
+	}
+
+	@Test
+	public void searchAndParam() {
+		Member findMember = queryFactory
+				.selectFrom(member)
+				.where(
+						member.username.eq("member1"),
+						(member.age.eq(10))
+				)
 				.fetchOne();
 
 		assertThat(findMember.getUsername()).isEqualTo("member1");
