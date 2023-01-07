b3f1bb1c1be7b90f4c97e6a6ebe9e591037df2c2
yh725 <yh725@kakao.com>
2023-01-07 23:47:28 +0900

    기본 문법 - 조인(기본 조인)

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -224,4 +224,40 @@ public class QuerydslBasicTest {
 		assertThat(teamB.get(team.name)).isEqualTo("teamB");
 		assertThat(teamB.get(member.age.avg())).isEqualTo(35); // (30 + 40) / 2
 	}
+
+	/**
+	 * 팀 A에 소속된 모든 회원
+	 */
+	@Test
+	public void join() {
+		List<Member> result = queryFactory
+				.selectFrom(member)
+				.join(member.team, team)
+				.where(team.name.eq("teamA"))
+				.fetch();
+
+		assertThat(result)
+				.extracting("username")
+				.containsExactly("member1", "member2");
+	}
+
+	/**
+	 * 세타 조인
+	 * 회원의 이름이 팀 이름과 같은 회원 조회
+	 */
+	@Test
+	public void theta_join() {
+		em.persist(new Member("teamA"));
+		em.persist(new Member("teamB"));
+
+		List<Member> result = queryFactory
+				.select(member)
+				.from(member, team)
+				.where(member.username.eq(team.name))
+				.fetch();
+
+		assertThat(result)
+				.extracting("username")
+				.containsExactly("teamA", "teamB");
+	}
 }
